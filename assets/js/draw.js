292c0eb39f9ca9c547a990af9bd48fc3b853effb
Stanislav Gurin <work.gurin@gmail.com>
2017-06-16 14:33:29 +0200

    Added bar chart

--- assets/js/draw.js
+++ assets/js/draw.js
@@ -1,3 +1,4 @@
+var histcatexplong;
 var request = new XMLHttpRequest();
 request.open('GET', 'https://api.fixer.io/latest');
 request.send();
@@ -54,6 +55,9 @@ request.onreadystatechange = function (e) {
 $(function () {
 
     $('#colorpicker').colorpicker();
+    $("#imagepicker").imagepicker({
+        show_label: true,
+    });
 
     $('#datepicker').datepicker({
         format: 'mm/dd/yyyy',
@@ -80,9 +84,150 @@ d3.select(".save").on("click", function () {
     saveSvgAsPng(d3.select('svg').node(), 'chart.png');
 });
 
-$('.draw').on('click', function () {
-    var color = document.getElementById('color').value;
+var color = document.getElementById('color').value;
 
+function drawStackedAreaChart(histcatexplong) {
+    Array.prototype.max = function () {
+        return Math.max.apply(null, this);
+    };
+
+    Array.prototype.min = function () {
+        return Math.min.apply(null, this);
+    };
+
+    var arrayY = [];
+    for (var hist in histcatexplong[0].values) {
+        arrayY.push(histcatexplong[0].values[hist][1]);
+    }
+
+    var minYValue = arrayY.min();
+    var maxYValue = arrayY.max();
+
+    nv.addGraph(function () {
+        var chart = nv.models.stackedAreaChart()
+            .useInteractiveGuideline(true)
+            .x(function (d) {
+                return d['x']
+            })
+            .y(function (d) {
+                return d['y']
+            })
+            .duration(300)
+            .showControls(false)
+            .clipEdge(true)
+            .color([color])
+            .forceY([minYValue, maxYValue])
+        ;
+        chart.xAxis
+            .tickFormat(function (d) {
+                return d3.time.format('%x')(new Date(d))
+            });
+        chart.yAxis.tickFormat(d3.format(',.4f'));
+
+        d3.select("svg").remove();
+        // d3.select('svg')
+
+        d3.select('#chart1')
+            .append("svg")
+            .datum(histcatexplong)
+            .transition().duration(300)
+            .call(chart);
+        nv.utils.windowResize(chart.update);
+        return chart;
+    });
+}
+
+function drawBarChart(histcatexplong) {
+
+    Array.prototype.max = function () {
+        return Math.max.apply(null, this);
+    };
+
+    Array.prototype.min = function () {
+        return Math.min.apply(null, this);
+    };
+
+    var arrayY = [];
+    for (var hist in histcatexplong[0].values) {
+        arrayY.push(histcatexplong[0].values[hist][1]);
+    }
+
+    var minYValue = arrayY.min();
+    var maxYValue = arrayY.max();
+
+    nv.addGraph(function () {
+        var chart = nv.models.multiBarChart()
+            .showControls(false)
+            .color([color])
+            .forceY([minYValue, maxYValue])
+        ;
+
+        chart.xAxis
+            .tickFormat(function (d) {
+                return d3.time.format('%x')(new Date(d))
+            });
+        chart.yAxis.tickFormat(d3.format(',.4f'));
+
+        d3.select("svg").remove();
+
+        d3.select('#chart1')
+            .append("svg")
+            .datum(histcatexplong)
+            .transition().duration(100)
+            .call(chart);
+
+        nv.utils.windowResize(chart.update);
+
+        return chart;
+    });
+}
+
+function allIn(responseArray) {
+    function finalToData(key, value) {
+        this.key = key;
+        this.values = value;
+    }
+
+    function valueToChart(x, y) {
+        this.x = new Date(x).getTime();
+        this.y = y;
+    }
+
+    var finalArray = [];
+    var keyArray = [];
+
+    //get keyArray
+    for (var ra in responseArray) {
+        var response = responseArray[ra];
+        var val = response.rates;
+        for (var j in val) {
+            var sub_key = j;
+            keyArray.push(sub_key);
+        }
+    }
+
+    keyArray = jQuery.unique(keyArray);
+    console.log(keyArray);
+
+    for (var ka in keyArray) {
+        var valueArray = [];
+        for (var ra in responseArray) {
+            var response = responseArray[ra];
+            var val = response.rates;
+            var sub_val = val[keyArray[ka]];
+            valueArray.push(new valueToChart(response.date, sub_val));
+        }
+
+        var object = new finalToData(keyArray[ka], valueArray);
+        finalArray.push(object);
+    }
+
+    console.log("finalArray: " + JSON.stringify(finalArray));
+
+    return finalArray;
+}
+
+$('#getData').click(function () {
     Date.prototype.addDays = function (days) {
         var dat = new Date(this.valueOf());
         dat.setDate(dat.getDate() + days);
@@ -111,165 +256,19 @@ $('.draw').on('click', function () {
     var dateFrom = document.getElementById('dateFrom').value;
     var dateTo = document.getElementById('dateTo').value;
     var dateArray = getDates(new Date(dateFrom), new Date(dateTo));
-    /*for (i = 0; i < dateArray.length; i ++ ) {
-     alert (getFormattedDate(dateArray[i]));
-     }*/
-    function allIn(responseArray) {
-        function valueToStackedAreaChart(date, rate) {
-            var valueToData = [];
-            valueToData[0] = new Date(date).getTime();
-            valueToData[1] = rate;
-            return valueToData;
-        }
 
-        function finalToData(key, value) {
-            this.key = key;
-            this.values = value;
-        }
-
-        function valueToLineWithFocusChart(x, y) {
-            this.x = new Date(x).getTime();
-            this.y = y;
-        }
-
-        var finalArray = [];
-        var keyArray = [];
-
-        //get keyArray
-        for (var ra in responseArray) {
-            var response = responseArray[ra];
-            var val = response.rates;
-            for (var j in val) {
-                var sub_key = j;
-                keyArray.push(sub_key);
-            }
-        }
-
-        keyArray = jQuery.unique(keyArray);
-        console.log(keyArray);
-
-        for (var ka in keyArray) {
-            var valueArray = [];
-            for (var ra in responseArray) {
-                var response = responseArray[ra];
-                var val = response.rates;
-                var sub_val = val[keyArray[ka]];
-                valueArray.push(valueToStackedAreaChart(response.date, sub_val));
-                // valueArray.push(new valueToLineWithFocusChart(response.date, sub_val));
-            }
-
-            // valueArray.push(new valueToLineWithFocusChart(responseArray[responseArray.length - 1].date, responseArray[0].rates[keyArray[ka]]));
-
-            var object = new finalToData(keyArray[ka], valueArray);
-            finalArray.push(object);
-        }
-
-        console.log("finalArray: " + JSON.stringify(finalArray));
-
-        return finalArray;
-    }
-
-    function drawStackedAreaChart(histcatexplong) {
-        var chart;
-
-        Array.prototype.max = function () {
-            return Math.max.apply(null, this);
-        };
-
-        Array.prototype.min = function () {
-            return Math.min.apply(null, this);
-        };
-
-        var arrayY = [];
-        for (var hist in histcatexplong[0].values) {
-            arrayY.push(histcatexplong[0].values[hist][1]);
-        }
-
-        var minYValue = arrayY.min();
-        var maxYValue = arrayY.max();
-
-        nv.addGraph(function () {
-            chart = nv.models.stackedAreaChart()
-                .margin({left: 100, right: 50})
-                .useInteractiveGuideline(true)
-                .x(function (d) {
-                    return d[0]
-                })
-                .y(function (d) {
-                    return d[1]
-                })
-                .duration(300)
-                .showControls(false)
-                .clipEdge(true)
-                .showLegend(false)
-                .color([color])
-                .forceY([minYValue, maxYValue])
-            ;
-            chart.xAxis
-                .tickFormat(function (d) {
-                    return d3.time.format('%x')(new Date(d))
-                });
-            chart.yAxis.tickFormat(d3.format(',.4f'));
-
-            d3.select('#chart1')
-                .datum(histcatexplong)
-                .transition().duration(300)
-                .call(chart);
-            nv.utils.windowResize(chart.update);
-            return chart;
-        });
-    }
-
-    function drawLineWithFocusChart(histcatexplong) {
-        nv.addGraph(function () {
-            var chart = nv.models.lineWithFocusChart()
-                .margin({"right": 50})
-                .useInteractiveGuideline(true)
-                .showLegend(false)
-                .color([color])
-            ;
-
-            chart.xAxis
-                .tickFormat(function (d) {
-                    return d3.time.format('%x')(new Date(d))
-                });
-
-            chart.x2Axis
-                .tickFormat(function (d) {
-                    return d3.time.format('%x')(new Date(d))
-                });
-
-            chart.yAxis
-                .tickFormat(d3.format(',.4f'));
-
-            chart.y2Axis
-                .tickFormat(d3.format(',.4f'));
-
-            chart.xScale(d3.time.scale.utc());
-
-            d3.select('#chart1')
-                .datum(histcatexplong)
-                .transition().duration(100)
-                .call(chart);
-
-            nv.utils.windowResize(chart.update);
-
-            return chart;
-        });
-    }
-
-    var responseArray = [];
     var base = document.getElementById('baseList').value;
     var symbol = document.getElementById('symbolList').value;
 
     var f = (function () {
         var xhr = [];
         var successfulRequests = 0;
+        var responseArray = [];
 
         for (i = 0; i < dateArray.length; i++) {
             (function (i) {
                 xhr[i] = new XMLHttpRequest();
-                url = 'http://api.fixer.io/' + getFormattedDate(dateArray[i]) + 'base=' + base + '&?symbols=' + symbol;
+                url = 'http://api.fixer.io/' + getFormattedDate(dateArray[i]) + '?base=' + base + '&symbols=' + symbol;
                 xhr[i].open("GET", url, true);
                 xhr[i].onreadystatechange = function () {
                     if (xhr[i].readyState === 4 && xhr[i].status === 200) {
@@ -281,9 +280,7 @@ $('.draw').on('click', function () {
                                 return new Date(a.date).getTime() - new Date(b.date).getTime()
                             });
 
-                            var histcatexplong = allIn(responseArray);
-                            drawStackedAreaChart(histcatexplong);
-                            // drawLineWithFocusChart(histcatexplong);
+                            histcatexplong = allIn(responseArray);
                         }
                     }
                 };
@@ -291,6 +288,23 @@ $('.draw').on('click', function () {
             })(i);
         }
     })();
+});
+
+$('.draw').click(function () {
+    var selectedGraph = $("#imagepicker").data('picker').selected_values()[0];
+    console.info("selectedGraph", selectedGraph)
+
+    if (selectedGraph == 1) {
+        drawStackedAreaChart(histcatexplong);
+    }
+    if (selectedGraph == 2) {
+        drawBarChart(histcatexplong);
+    }
 
     return false;
+});
+
+
+$('.clear').click(function () {
+    d3.select("svg").remove();
 });
